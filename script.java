6a2de17386464160346b01b5c1bb9f93d0ab7000
luluksyarifatulu1 <lutviana.zahriani1@smk.belajar.id>
2024-11-13 12:10:02 +0700

    DONE

---
+++ script.java
@@ -0,0 +1,9 @@
+document.querySelectorAII('a[href^="#]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
